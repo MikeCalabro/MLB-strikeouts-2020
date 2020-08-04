af517518cdaa86dacc8fa8cceddc7ec329208085
Michael Calabro <michael_calabro@college.harvard.edu>
2020-08-04 11:21:32 -0400

    new data

--- data_playground.R
+++ data_playground.R
@@ -67,3 +67,86 @@ dbGetQuery(con,
 
 dbDisconnect(con)
 
+
+library(baseballr)
+
+pitch_2015 <- scrape_statcast_savant_batter_all(start_date = "2015-05-01",
+                                              end_date = "2015-05-07") %>%
+  select(pitch_type,
+         release_speed,
+         player_name,
+         game_year,
+         launch_speed,
+         events)
+
+event_list <- c("single", "double", "triple", "home_run")
+
+pitch_2015 %>%
+  filter(launch_speed > 0) %>%
+  filter(events %in% event_list) %>%
+  ggplot(aes(x = factor(events, level = event_list))) +
+  geom_violin(aes(y = launch_speed, color = events),
+              draw_quantiles = 0.5)
+
+advanced_stats <- function(year) {
+  return(
+    scrape_statcast_savant_batter_all(start_date = sprintf("%s-05-01", year),
+                                      end_date = sprintf("%s-05-07", year)) %>%
+      select(pitch_type,
+             release_speed,
+             player_name,
+             game_year,
+             launch_speed,
+             events)
+  )
+}
+
+pitch_2016 <- advanced_stats("2016")
+pitch_2017 <- advanced_stats("2017")
+pitch_2018 <- advanced_stats("2018")
+pitch_2019 <- advanced_stats("2019")
+
+pitch_2014 <- advanced_stats("2014")
+
+advanced_data <- pitch_2015 %>%
+  full_join(pitch_2016) %>%
+  full_join(pitch_2017) %>%
+  full_join(pitch_2018) %>%
+  full_join(pitch_2019)
+
+advanced_data %>%
+  filter(launch_speed > 0) %>%
+  filter(events %in% event_list) %>%
+  ggplot(aes(x = factor(events, level = event_list))) +
+  geom_violin(aes(y = launch_speed, color = events),
+              draw_quantiles = 0.5)
+
+advanced_data %>%
+  filter(pitch_type == "FF") %>%
+  ggplot(aes(x = factor(game_year))) +
+  geom_boxplot(aes(y = release_speed),
+              draw_quantiles = 0.5)
+
+advanced_data %>%
+  group_by(game_year, events) %>%
+  summarise(count = n()) %>%
+  filter(events == "strikeout")
+
+advanced_data %>%
+  group_by(pitch_type, events) %>%
+  summarise(count = n()) %>%
+  filter(events == "strikeout")
+
+advanced_data %>%
+  group_by(game_year, pitch_type, events) %>%
+  summarise(count = n()) %>%
+  filter(events == "strikeout") %>%
+  filter(pitch_type == "SL")
+
+advanced_data %>%
+  group_by(game_year, pitch_type) %>%
+  summarise(count = n(), avg_speed = mean(release_speed)) %>%
+  filter(pitch_type %in% c("SL", "FF")) %>%
+  arrange(pitch_type)
+
+
